cab8a2eab880ffd143e40e85cffce2e76b24bca6
Vuyisile Ndlovu <terrameijar@gmail.com>
2019-07-05 22:23:22 +0200

    Added List view model, view and urls

--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Invoice(models.Model):
+    title = models.CharField(max_length=200)
+    # user
+    # client
+    body = models.TextField()
+    invoice_total = models.DecimalField(max_digits=6, decimal_places=2)
+    create_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_invoice_total(self):
+        return f'${self.invoice_total}'
--- invoices/views.py
+++ invoices/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
+from .models import Invoice
+
 
 class InvoiceListView(ListView):
-    model = ''
-    template_name = ''
+    model = Invoice
+    template_name = 'home.html'
