92da18707ffa575c34d63e8c99d7c0fbcc3dfa91
Vuyisile Ndlovu <terrameijar@gmail.com>
2019-12-04 12:55:47 +0200

    Add Invoice line item logic

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Invoice, Client
+from .models import Invoice, Client, InvoiceItems
 
 
-admin.site.register(Invoice)
+
+
+class InvoiceItemsInline(admin.TabularInline):
+    model = InvoiceItems
+
+
+class InvoiceAdmin(admin.ModelAdmin):
+    inlines = [
+        InvoiceItemsInline,
+    ]
+    readonly_fields = ('invoice_total',)
+
+
+admin.site.register(Invoice, InvoiceAdmin)
 admin.site.register(Client)
+# admin.site.register(InvoiceItems)
--- invoices/models.py
+++ invoices/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db import models
 from django.urls import reverse
 
@@ -23,15 +24,14 @@ class Client(models.Model):
 
     def __repr__(self):
         return f'Client: {self.first_name} {self.last_name}'
-
-
 class Invoice(models.Model):
     title = models.CharField(max_length=200)
     # user
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    body = models.TextField()
-    invoice_total = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    invoice_total = models.DecimalField(max_digits=6, decimal_places=2, blank=True, editable=False)
     create_date = models.DateField(auto_now_add=True)
+    # line_item = models.ForeignKey(InvoiceItems, on_delete=models.CASCADE, blank=True)
 
     def get_absolute_url(self):
         return reverse('invoice-detail', kwargs={'pk': self.pk})
@@ -43,4 +43,30 @@ class Invoice(models.Model):
         return f'<Invoice: {self.client} - {self.title}>'
 
     def get_invoice_total(self):
-        return f'${self.invoice_total}'
+        # return f'${self.invoice_total}'
+        total = Decimal('0.00')
+        total = sum([item.subtotal() for item in self.items.all()])
+        self.invoice_total = total
+
+    def save(self, *args, **kwargs):
+        self.get_invoice_total()
+        super(Invoice, self).save(*args, **kwargs)
+
+class InvoiceItems(models.Model):
+    # Invoice Line Items
+    invoice = models.ForeignKey(Invoice, related_name='items', on_delete=models.CASCADE)
+    item = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
+    rate = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        # return self.item
+        return f'{self.item} - {self.subtotal()}'
+
+    def __repr__(self):
+        return f'<Invoice Line Item: {self.item} - {self.subtotal()}>'
+
+    def subtotal(self):
+        return self.quantity * self.rate
+
+
