98bd1520b4d20d58c60470dd402cd81df0c8443a
Vuyisile Ndlovu <terrameijar@gmail.com>
2020-01-17 11:07:18 +0200

    Fix styling in invoice create view

--- invoices/tests/test_UI.py
+++ invoices/tests/test_UI.py
@@ -1,32 +1,32 @@
 from django.test import LiveServerTestCase
+import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 
 
-class TestLandingPage(StaticLiveServerTestCase):
+class TestInvoicingApp(StaticLiveServerTestCase):
 
     def setUp(self):
         self.base_url = self.live_server_url
-
         self.driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver') # Add chrome driver to sys PATH first
 
     def test_landing_page_load(self):
         self.driver.get(self.base_url)
         self.assertEqual(self.driver.title, "Invoices Landing Page")
-        # self.assertIn("localhost", self.driver.current_url)
 
-
-    # def test_landing_page_links(self):
-        # self.driver.get(self.base_url)
-        # self.assertEqual(self.driver.title, "Invoices Landing Page")
-        # self.assertIn("http://127.0.0.1:8000/", self.driver.current_url)
+    def test_new_invoice_can_be_created(self):
+        self.driver.get(self.base_url)
+        invoices_link = self.driver.find_element_by_id("invoices-dropdown")
+        invoices_link.click()
+        # time.sleep(10)
 
 
 
 
     def tearDown(self):
         self.driver.quit()
+
 
 
 
