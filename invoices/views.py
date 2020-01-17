4103df755711b150343a21610f89d5efadd46c0e
Vuyisile Ndlovu <terrameijar@gmail.com>
2020-01-17 11:41:58 +0200

    Limit clients user can view in invoice create view

--- invoices/views.py
+++ invoices/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse, reverse_lazy
-from django.forms import formset_factory, modelformset_factory
 from django.forms.models import inlineformset_factory
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Invoice, Client, InvoiceItem
-from .forms import InvoiceItemsForm
+from .forms import InvoiceCreateForm
 
 InvoiceItemsFormset = inlineformset_factory(
     Invoice, InvoiceItem, fields=('item', 'quantity', 'rate',),
@@ -15,7 +13,7 @@ InvoiceItemsFormset = inlineformset_factory(
 
 
 class InvoiceListView(LoginRequiredMixin, ListView):
-    # model = Invoice --> Thi is the same as Invoice.objects.all()
+    # model = Invoice --> This the same as Invoice.objects.all()
 
     template_name = 'dashboard.html'
 
@@ -52,13 +50,14 @@ class InvoiceDetailView(LoginRequiredMixin, DetailView):
 class InvoiceCreateView(LoginRequiredMixin, CreateView):
     model = Invoice
     template_name = 'new_invoice.html'
-    fields = ('title', 'client')
+    form_class = InvoiceCreateForm
 
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            return Client.objects.filter(created_by=self.request.user)
-        else:
-            return Client.objects.none()
+    def get_form_kwargs(self):
+        # The queryset for this view was created in the form
+        # get_queryset() isn't used in create views
+        kwargs = super(InvoiceCreateView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
 
     def get_context_data(self, **kwargs):
         # To display the invoice items
@@ -73,7 +72,6 @@ class InvoiceCreateView(LoginRequiredMixin, CreateView):
         return data
 
     def form_valid(self, form):
-        # TODO: Add a client queryset here Only allow user to select clients linked to them
         form.instance.user = self.request.user
         context = self.get_context_data()
         invoice_items = context['invoice_items']
@@ -171,72 +169,3 @@ class ClientUpdateView(LoginRequiredMixin, UpdateView):
             return Client.objects.filter(created_by=self.request.user)
         else:
             return Client.objects.none()
-
-
-
-# def invoice_create_view(request):
-#     # https://docs.djangoproject.com/en/dev/topics/forms/modelforms/#inline-formsets
-#     # https://github.com/elo80ka/django-dynamic-formset
-#     # https://collingrady.wordpress.com/2008/02/18/editing-multiple-objects-in-django-with-newforms/
-#     # https://stackoverflow.com/questions/18413303/how-can-i-have-the-equivalent-of-djangos-admin-inlines-in-a-modelform
-#     # testFormSet = modelformset_factory(InvoiceItems, form=InvoiceItemsForm, fields=('invoice','item', 'quantity'), extra=0)
-#     # formset = testFormSet()
-#     # context = {
-#     #     'formset': formset,
-#     # }
-#     if request.method == 'POST':
-#         InvoiceItemsFormset = inlineformset_factory(
-#             Invoice, InvoiceItems, fields=('item', 'quantity', 'rate', 'tax')
-#         )
-#         invoice = Invoice(request.POST)
-#         formset = InvoiceItemsFormset(request.POST)
-#         if invoice.is_valid() and formset.is_valid():
-#             invoice_instance = invoice.save()
-#             invoice_items = formset.save(commit=False)
-#             for invoice_item in invoice_items:
-#                 invoice_item.invoice_id = invoice_instance.id
-#                 invoice_item.save()
-#             invoice.save()
-#             formset = InvoiceItemsFormset(queryset=InvoiceItems.objects.filter(invoice__id=invoice.pk))
-#     else:
-#         invoice = Invoice()
-#         # invoice.save()
-#         InvoiceItemsFormset = inlineformset_factory(
-#             Invoice, InvoiceItems, fields=('invoice','item', 'quantity', 'rate', 'tax')
-#         )
-#         formset = InvoiceItemsFormset()
-
-
-
-#     context ={
-#         'formset': formset,
-#         # 'form': invoice,
-#     }
-#     return render(request,'new_invoice_test.html', context)
-
-# def edit_invoice(request, invoice_id):
-#     # https://github.com/elo80ka/django-dynamic-formset
-#     # https://simpleit.rocks/python/django/dynamic-add-form-with-add-button-in-django-modelformset-template/
-#     # https://medium.com/all-about-django/adding-forms-dynamically-to-a-django-formset-375f1090c2b0
-#     invoice = Invoice.objects.get(pk=invoice_id)
-#     InvoiceItemsFormset = modelformset_factory(InvoiceItem, fields=('item', 'quantity','rate','tax',))
-#
-#     if request.method == 'POST':
-#         formset = InvoiceItemsFormset(request.POST, queryset=InvoiceItem.objects.filter(invoice__id=invoice.pk))
-#         if formset.is_valid():
-#             instances = formset.save(commit=False)
-#             for instance in  instances:
-#                 instance.invoice_id = invoice.id
-#                 instance.save()
-#             invoice.save() # Do this in order to update invoice total
-#     # else:
-#     #     # Create a new invoice here
-#     #     # https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/#saving-objects-in-the-formset
-#     #     formset = InvoiceItemsFormset(request.POST)
-#     #     invoice = Invoice()
-#     #     if formset.is_valid():
-#     #         instances = formset.save(commit=False)
-#
-#
-#     formset = InvoiceItemsFormset(queryset=InvoiceItem.objects.filter(invoice__id=invoice.pk))
-#     return render(request, 'index.html', {'formset' : formset})
